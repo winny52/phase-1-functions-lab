296eb7b761eaf3154abe575eed76601beede8085
WinnySigilai <winnysigilai02@gmail.com>
2023-06-21 11:38:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    if(pickup > 42) {
+        const distance = pickup - 42;
+        return distance;
+    }
+    else if(pickup < 42) {
+        const distance = 42 - pickup;
+        return distance;
+    }
+}
+
+
+function distanceFromHqInFeet(hq) { 
+     const feet = distanceFromHqInBlocks(hq) * 264;
+     return feet;    
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if(destination > start){
+        const dist = destination - start;
+        return dist * 264;
+    } else if (destination < start){
+        const dist = start -destination;
+        return dist * 264;
+    }
+
+}
+
+function calculatesFarePrice(start, destination) {
+    const distTravelled =  distanceTravelledInFeet(start, destination);
+    if(distTravelled <= 400){
+        const fareDollar = 0;
+        return fareDollar;
+    }
+    else if(distTravelled >400 && distTravelled < 2000) {
+        const fare = (distTravelled - 400) * 2;
+        const fareDollar = fare/100;
+        return fareDollar
+    }
+    else if(distTravelled > 2000 && distTravelled <= 2500){
+        const fareDollar = 25;
+        return fareDollar;
+    }
+    else if(distTravelled > 2500){
+        return 'cannot travel that far';
+    }
+  }
+console.log(calculatesFarePrice(43, 44));
